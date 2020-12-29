6d557133f16e84e8902f87a70a1598c94ece6244
Jarosław Armatys <jarmatys@luxbit.com.pl>
2020-12-29 15:50:39 +0100

    bug fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import {
 import thunk from "redux-thunk";
 import reducers from "./reducers"
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : f => f;
 
 const store = createStore(reducers, compose(applyMiddleware(thunk), devTools));
 
